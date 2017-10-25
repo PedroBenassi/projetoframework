46bca794b1526e6824bc53ed5a9f34697434b5fd
thagui.oliveira <thagui.oliveira@gmail.com>
2017-10-25 16:00:31 -0200

    edição forms

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,46 @@
+var cadastroInstituto = '<div class="col-md-12" >'+
+  '<button type="button" class="btn btn-collapse" data-toggle="collapse" data-target="#predio">Prédio</button>'+
+  '<i class="fa fa-plus icon-plus" aria-hidden="true" id="predio-plus"></i>'+
+  '<br><br>'+
+'</div>'+
+'<div class="collapse" id="predio">'+
+  '<div class="col-md-10" class="collapse">'+
+   '<div class="form-group">'+
+    '<input type="text" class="form-control login-input" id="nome-predio" placeholder="Nome" required>'+
+   '</div>'+
+ '</div>'+
+ '<div class="col-md-3">'+
+   '<div class="form-group">'+
+    '<input type="text" class="form-control login-input" id="sigla-predio" placeholder="Sigla" required>'+
+   '</div>'+
+ '</div>'+
+ '<div class="col-md-3">'+
+   '<div class="form-group">'+
+    '<input type="text" class="form-control login-input" id="lat-predio" placeholder="Latitude" required>'+
+   '</div>'+
+ '</div>'+
+ '<div class="col-md-3">'+
+   '<div class="form-group">'+
+    '<input type="text" class="form-control login-input" id="long-predio" placeholder="Longitude" required>'+
+   '</div>'+
+   '<br>'+
+ '</div>'+
+ '<div class="col-md-12">'+
+   '<button type="button" class="btn btn-collapse" data-toggle="collapse" data-target="#sala">Sala</button>'+
+   '<i class="fa fa-plus icon-plus" aria-hidden="true"></i>'+
+   '<br><br>'+
+ '</div>'+
+ '<div class="collapse" id="sala">'+
+   '<div class="col-md-10">'+
+     '<div class="form-group">'+
+      '<input type="text" class="form-control login-input" id="nome-sala" placeholder="Nome" required>'+
+     '</div>'+
+   '</div>'+
+ '</div>'+
+'</div>';
 
+$(document).ready(function(){
+  $("#predio-plus").click(function(){
+    $(cadastroInstituto).appendTo(".content-predio");
+});
+});
